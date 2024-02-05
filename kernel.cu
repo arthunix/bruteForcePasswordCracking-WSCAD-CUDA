32d49114d22a3f44ba39cd9c08698dd2b6a13846
Arthur Silverio <thursilverio@gmail.com>
2024-02-05 15:41:43 -0300

    update: use a global flag to terminate and use static local

--- kernel.cu
+++ kernel.cu
@@ -5,7 +5,7 @@
 #include "md5.cuh"
 #include "device_launch_parameters.h"
 
-#define MAX 5
+#define MAX 7
 #define LETTERS_LEN 36
 
 typedef unsigned char byte;
@@ -16,7 +16,6 @@ __device__ __constant__ byte dHash1[MD5_DIGEST_LENGTH];
 
 void strHex_to_byte(char*, byte*);
 __device__ int dstrncmp(const char*, const char*, size_t);
-__device__ size_t dstrlen(const char*);
 __device__ __host__ void print_digest(byte*);
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
@@ -29,8 +28,8 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
 	}
 }
 
-__global__ void iterate(int len) {
-	char* str = new char[len];
+__global__ void iterate(int len, int* dOk) {
+	char str[MAX];
 	byte hash2[MD5_DIGEST_LENGTH];
 	long long index, divisor;
 
@@ -43,11 +42,13 @@ __global__ void iterate(int len) {
 
 	//printf("idx: %lld.str: %s\n", index, str);
 
-	if (dstrlen(str)) {
-		MD5((byte*)str, dstrlen(str), (unsigned int*)hash2);
-		if (dstrncmp((char*)dHash1, (char*)hash2, MD5_DIGEST_LENGTH) == 0) {
-			printf("found: %s\n", str);
-		}
+	MD5((byte*)str, len, (unsigned int*)hash2);
+	if (dstrncmp((char*)dHash1, (char*)hash2, MD5_DIGEST_LENGTH) == 0) {
+		printf("found: ");
+		for(int i = 0; i < len; i++)
+			printf("%c", str[i]);
+		printf("\n");
+		*dOk = 1;
 	}
 	delete[] str;
 }
@@ -57,6 +58,7 @@ int main(int argc, char** argv) {
 	int len, r;
 	char hash1_str[2 * MD5_DIGEST_LENGTH + 1];
 	byte hash1[MD5_DIGEST_LENGTH]; // password hash
+	int *dOk;
 
 	// Input:
 	r = scanf("%s", hash1_str);
@@ -73,15 +75,21 @@ int main(int argc, char** argv) {
 
 	gpuErrchk( cudaMemcpyToSymbol(dLetters, &letters, LETTERS_LEN, 0, cudaMemcpyHostToDevice) );
 	gpuErrchk( cudaMemcpyToSymbol(dHash1, &hash1, MD5_DIGEST_LENGTH, 0, cudaMemcpyHostToDevice) );
+	gpuErrchk( cudaMallocManaged(&dOk, 1*sizeof(int)) );
+	*dOk = 0;
 
 	// Generate all possible passwords of different sizes.
-	for (len = 1; len <= lenMax; len++) {
+	for (len = 1; len <= lenMax && *dOk != 1; len++) {
 		dim3 gridArch = { (unsigned int)pow(LETTERS_LEN, len-1), 1, 1 };
 		dim3 blckArch = { LETTERS_LEN, 1, 1 };
 
-		iterate <<< gridArch, blckArch >>> (len);
-		gpuErrchk(cudaPeekAtLastError());
+		iterate <<< gridArch, blckArch >>> (len, dOk);
+
+		gpuErrchk( cudaPeekAtLastError() );
+		gpuErrchk( cudaDeviceSynchronize() );
 	}
+
+	gpuErrchk( cudaFree(dOk) );
 }
 
 void strHex_to_byte(char* str, byte* hash) {
@@ -108,12 +116,6 @@ __device__ int dstrncmp(const char* _l, const char* _r, size_t n) {
 	ll = (*(unsigned char*)_l);
 	lr = (*(unsigned char*)_r);
 	return ((ll < lr) ? -1 : (ll > lr));
-}
-
-__device__ size_t dstrlen(const char* str) {
-	const char* s;
-	for (s = str; *s; ++s);
-	return(s - str);
 }
 
 __device__ __host__ void print_digest(byte* hash) {
